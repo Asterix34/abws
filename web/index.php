75531a5a6e2f2de393344abbc4725fb5c1ad7ef3
HumanBooster <bclerc@humanbooster.com>
2015-02-19 13:03:10 +0100

    added DB support

--- web/index.php
+++ web/index.php
@@ -12,23 +12,46 @@ require_once __DIR__."/../vendor/autoload.php";
 // we load a new Silex app
 $app = new Silex\Application();
 
+// we link it to a DB
+$dbParams = array(
+	'dbname' => 'abws',
+	'user' => 'abws',
+	'password' => 'password',
+	'host' => 'localhost',
+	'driver' => 'pdo_mysql'
+);
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array('db.options' => $dbParams));
+
+
 // debug
 $app['debug'] = DEBUG;
 $params = array();
 
 // Get some credentials with predef duration
-$app->get(PREFIX."/getCredentials", function () use ($params) {
-	return getCredentials();
+$app->get(PREFIX."/getCredentials", function () use ($app, $params) {
+	return getCredentials($app);
 });
 
-$app->get(PREFIX."/getCredentials/{duration}", function ($duration) use ($params) {
-	return getCredentials($duration);
+$app->get(PREFIX."/getCredentials/{duration}", function ($duration) use ($app, $params) {
+	return getCredentials($app, $duration);
 });
 
 $app->run();
 
 // functions
-function getCredentials($duration = DEFAULT_DURATION) {
-	return "Requested new credentials for ".$duration." seconds";
+function getCredentials($app, $duration = DEFAULT_DURATION) {
+	$cp = new CredentialsProvider();
+	$creds = new Credentials("michel", "1234", time() + $duration);
+	return $app->json($creds);
 
+}
+
+// entity Credentials
+class Credentials {
+	public $login, $password, $validUntil;
+	function __construct($l, $p, $v) {
+		$this->login = $l;
+		$this->password = $p;
+		$this->validUntil = $v;
+	}
 }
